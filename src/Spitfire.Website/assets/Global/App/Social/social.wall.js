c1a95509322a498dca4a298da0a26ad2918280fd
Johannes <jzi@sitecore.net>
2015-08-06 11:33:26 +0100

    Added event overview page Added news overview page Updated styles Added adverts

---
+++ src/Spitfire.Website/assets/Global/App/Social/social.wall.js
@@ -0,0 +1,30 @@
+﻿jQuery(document).ready(function ($) {
+    $('#social-stream').dcSocialStream({
+        feeds: {
+            //twitter: {
+            //    url: '/Content/plugins/social-stream/twitter.php',
+            //    id: 'scofficecore'
+            //},
+            facebook: {
+                id: '124849824292539 ',
+                out: 'intro,thumb,text,user,share'
+            }//,
+            //youtube: {
+            //    id: 'SitecoreUKTV',
+            //    thumb: '0'
+            //}
+        },
+        rotate: {
+            delay: 0
+        },
+        twitterId: 'scofficecore',
+        control: false,
+        filter: true,
+        wall: true,
+        cache: false,
+        max: 'limit',
+        limit: 10,
+        iconPath: '/Content/images/social-stream/dcsns-dark/',
+        imagePath: '/Content/images/social-stream/dcsns-dark/'
+    });
+});
